91b0556e3220528a504ab1679ce45f43f3382add
fiveddd <qiudaoying@qp197.com>
2017-06-05 16:10:39 +0800

    [added] new library

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='QueryVin',
-      version='0.0.1',
+      version='0.0.2',
       description='query vin information',
       url='https://github.com/fiveddd/QueryVin',
       author='FiveDDD',
